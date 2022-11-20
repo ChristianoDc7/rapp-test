1a296f52463b2db3f0460535ba993378c1659ae6
ChristianoDc7 <christianod97@gmail.com>
2022-11-21 01:12:53 +0300

    add typewriter effect

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,15 +1,31 @@
 import React from "react";
 import { ArrowDown } from "../assets/icons/ArrowDown";
+import Typewriter from 'typewriter-effect';
 
 export const Hero = () => {
     return (
         <div className="h-full flex justify-evenly flex-col">
             <h1 className="font-bold text-center">
-                We make people <br /> more valuable to Brands
+                    <Typewriter
+                            options={{
+                            autoStart: true,
+                            loop: true,
+                            delay: "natural",
+                             }}
+                            onInit={(typewriter) => {
+                            typewriter
+                                .typeString(`We make people`)
+                                .pauseFor(700)
+                                .deleteAll()
+                                .typeString(`more valuable to Brands`)
+                                .start();
+                            }}
+                        /> 
             </h1>
             <div onClick={()=>window.scrollTo({top: document.body.scrollHeight, behavior: 'smooth'})} className="arrow-wrap self-center cursor-pointer">
                 <ArrowDown className="arrow-down"/>
             </div>
+            
         </div>
     );
 };
